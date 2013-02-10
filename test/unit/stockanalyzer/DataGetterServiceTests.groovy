e5d9f414292bf5fcc5db36fa7347a10d9cacf463
Mike Graziano <mjgrazi@gmail.com>
2013-02-09 23:59:59 -0500

    refactoring

---
+++ test/unit/stockanalyzer/DataGetterServiceTests.groovy
@@ -0,0 +1,17 @@
+package stockanalyzer
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(DataGetterService)
+class DataGetterServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
